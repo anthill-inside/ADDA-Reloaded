28ec5a0da37fffb198ac2be91ed50aa40a143def
anthill-inside <ohmnomnom90@gmail.com>
2021-02-07 17:49:41 +0300

    Adde tilemap and a basic charachter controller
    End of the day 1. Work took plenty of time but that was expected considering my lack of familiarity with the Godot

---
+++ ADDA-Reloaded/Scripts/Player.gd
@@ -0,0 +1,40 @@
+
+extends KinematicBody2D
+
+export var  speed = 200
+export var rotation_speed = 1.5
+
+var velocity = Vector2.ZERO
+var rotation_dir = 0
+
+var anim = "Idle"
+
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed('Right'):
+		rotation_dir += 1
+	if Input.is_action_pressed('Left'):
+		rotation_dir -= 1
+	if Input.is_action_pressed('Down'):
+		velocity -= transform.x * speed
+	if Input.is_action_pressed('Up'):
+		velocity += transform.x * speed
+
+func _physics_process(delta):
+	get_input()
+	
+	if velocity == Vector2.ZERO:
+		anim = "Idle"
+	else:
+		if velocity.length() < 1.0:
+			velocity = 0
+			
+		if velocity.length() > 0.0:
+			anim = "Walk"
+		else:
+			anim = "Idle"
+		
+	$AnimatedSprite.play(anim);
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
